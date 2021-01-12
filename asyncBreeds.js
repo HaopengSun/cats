70bf20f40c4811ba253df005c00971a08c902a50
HaopengSun <sunhaopeng@gmail.com>
2021-01-12 15:51:01 +0000

    callback function used in async

--- asyncBreeds.js
+++ asyncBreeds.js
@@ -1,18 +1,35 @@
+// We saw why and how asynchronous functions such as readFile,
+// and our function breedDetailsFromFile, cannot simply return their data.
+// Instead they must use callback functions to pass that data back.
+
 const fs = require('fs');
 
-const breedDetailsFromFile = function(breed) {
+const breedDetailsFromFile = function(breed, callback) {
   console.log('breedDetailsFromFile: Calling readFile...');
   fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
     console.log("In readFile's Callback: it has the data.");
-    // ISSUE: Returning from *inner* callback function, not breedDetailsFromFile.
-    if (!error) return data;
+    if (!error) callback(data);
   });
-  // ISSUE: Attempting to return data out here will also not work.
-  // Currently not returning anything from here,
-  // so breedDetailsFromFile function returns undefined.
 };
 
-// we try to get the return value
-const bombay = breedDetailsFromFile('Bombay');
+const show = (bombay) => console.log('Return Value: ', bombay);
+const bombay = breedDetailsFromFile('Bombay', show);
+
+const fs = require('fs');
+
+// const breedDetailsFromFile = function(breed) {
+//   console.log('breedDetailsFromFile: Calling readFile...');
+//   fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
+//     console.log("In readFile's Callback: it has the data.");
+//     // ISSUE: Returning from *inner* callback function, not breedDetailsFromFile.
+//     if (!error) return data;
+//   });
+//   // ISSUE: Attempting to return data out here will also not work.
+//   // Currently not returning anything from here,
+//   // so breedDetailsFromFile function returns undefined.
+// };
+
+// // we try to get the return value
+// const bombay = breedDetailsFromFile('Bombay');
+// console.log('Return Value: ', bombay);
 // => will NOT print out details, instead we will see undefined!
-console.log('Return Value: ', bombay); 
